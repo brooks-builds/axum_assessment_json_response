cb112374ebe40f39a587489599bd5550bb0afeaf
Brookzerker <brooks@brookzerker.com>
2023-07-02 13:54:19 -0600

    created solution

--- src/router/favorite_languages.rs
+++ src/router/favorite_languages.rs
@@ -1,1 +1,16 @@
-pub async fn favorite_languages() {}
+use axum::Json;
+use serde::{Deserialize, Serialize};
+
+const LANGUAGE_LIST: &str = include_str!("../../favorite_languages.json");
+
+#[derive(Serialize, Deserialize)]
+pub struct FavoriteLanguage {
+    pub name: String,
+    pub popularity: f32,
+}
+
+pub async fn favorite_languages() -> Json<Vec<FavoriteLanguage>> {
+    let languages: Vec<FavoriteLanguage> = serde_json::from_str(LANGUAGE_LIST).unwrap();
+
+    Json(languages)
+}
